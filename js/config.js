6e5bb925a81765eaad5e8050cebffd2f61847c23
Bernardo <berramuspegonzalez@gmail.com>
2025-11-08 05:55:02 -0300

    Agregar configuración de URLs externas

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// Configuración de URLs externas
+const AppConfig = {
+    // URL del sistema de reservas
+    reservasUrl: 'https://reserva.great-site.net/',
+    
+    // URL del sistema de gestión escolar
+    sistemaUrl: './sge-no-disponible',
+    
+    // URL del formulario de inscripción
+    inscripcionUrl: ''
+};
+
+// Hacer la configuración disponible globalmente
+window.AppConfig = AppConfig;
